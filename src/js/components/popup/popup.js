45983d76f0e1bab5057720648b86ba9e575ddcf2
oleg levchuk <levchuk.oleg21@gmail.com>
2026-06-02 15:14:29 +0300

    add comments for Popup component

--- src/js/components/popup/popup.js
+++ src/js/components/popup/popup.js
@@ -1,10 +1,29 @@
+/**
+ * Popup configuration options
+ * @typedef {Object} PopupOptions
+ * @property {string} [popupHolderSelector] - Root element selector
+ * @property {string} [popupSelector] - Popup element selector
+ * @property {string} [openBtnSelector] - Selector for the open button
+ * @property {string} [closeBtnSelector] - Selector for the close button
+ * @property {string} [popupActiveClass] - Class added to popup holder when open
+ * @property {boolean} [lockScroll] - Lock body scroll when popup is open
+ * @property {boolean} [closeOnClickOutside] - Close popup on outside click
+ * @property {boolean} [closeOnEsc] - Close popup on Escape key
+ * @property {boolean} [accessibility] - Enable aria attributes
+ * @property {Function|null} [onOpen] - Callback on popup open
+ * @property {Function|null} [onClose] - Callback on popup close
+ * @property {number|null} [destroyAbove] - Disable popup above this width (px)
+ * @property {number|null} [destroyBelow] - Disable popup below this width (px)
+ */
+
 export default class Popup {
   constructor(options = {}) {
+    // User options
     this.popupHolderSelector = options.popupHolderSelector || ".popup-holder";
     this.popupSelector = options.popupSelector || ".popup";
-    this.popupActiveClass = options.popupActiveClass || "popup-active";
     this.openBtnSelector = options.openBtnSelector || ".popup-open";
     this.closeBtnSelector = options.closeBtnSelector || ".popup-close";
+    this.popupActiveClass = options.popupActiveClass || "popup-active";
     this.lockScroll = options.lockScroll ?? false;
     this.closeOnClickOutside = options.closeOnClickOutside ?? true;
     this.closeOnEsc = options.closeOnEsc ?? true;
@@ -14,12 +33,16 @@ export default class Popup {
     this.destroyAbove = options.destroyAbove || null;
     this.destroyBelow = options.destroyBelow || null;
 
+    // DOM elements
     this.popupHolder = document.querySelector(this.popupHolderSelector);
     this.popup = null;
     this.openBtn = null;
 
+    // Flag
     this.isInitialized = false;
   }
+
+  // Initialization
 
   init() {
     if (!this.popupHolder) {
@@ -44,6 +67,8 @@ export default class Popup {
     this.isInitialized = true;
   }
 
+  // Setup
+
   findElements() {
     this.popup = this.popupHolder.querySelector(this.popupSelector);
     this.openBtn = this.popupHolder.querySelector(this.openBtnSelector);
@@ -58,6 +83,48 @@ export default class Popup {
     this.popup.setAttribute("aria-modal", "true");
     this.popup.setAttribute("tabindex", "-1");
   }
+
+  // Core Logic
+  // Main popup behavior (open/close/toggle)
+
+  togglePopup() {
+    const isActive = this.popupHolder.classList.contains(this.popupActiveClass);
+
+    isActive ? this.closePopup() : this.openPopup();
+  }
+
+  openPopup() {
+    if (this.lockScroll) {
+      document.body.style.overflow = "hidden";
+    }
+
+    if (this.accessibility) {
+      this.openBtn.setAttribute("aria-expanded", "true");
+      this.popup.focus();
+    }
+
+    this.popupHolder.classList.add(this.popupActiveClass);
+
+    this.onOpen?.();
+  }
+
+  closePopup() {
+    if (this.lockScroll) {
+      document.body.style.removeProperty("overflow");
+    }
+
+    if (this.accessibility) {
+      this.openBtn.setAttribute("aria-expanded", "false");
+      this.openBtn.focus();
+    }
+
+    this.popupHolder.classList.remove(this.popupActiveClass);
+
+    this.onClose?.();
+  }
+
+  // Events
+  // All event listeners (user interaction)
 
   bindEvents() {
     this.handleClick = (e) => {
@@ -105,6 +172,9 @@ export default class Popup {
     window.addEventListener("resize", this.handleBreakpointResize);
   }
 
+  // Responsive
+  // Handles breakpoint-based behavior
+
   checkBreakpoint() {
     const width = window.innerWidth;
 
@@ -122,41 +192,8 @@ export default class Popup {
     }
   }
 
-  togglePopup() {
-    const isActive = this.popupHolder.classList.contains(this.popupActiveClass);
-
-    isActive ? this.closePopup() : this.openPopup();
-  }
-
-  openPopup() {
-    if (this.lockScroll) {
-      document.body.style.overflow = "hidden";
-    }
-
-    if (this.accessibility) {
-      this.openBtn.setAttribute("aria-expanded", "true");
-      this.popup.focus();
-    }
-
-    this.popupHolder.classList.add(this.popupActiveClass);
-
-    this.onOpen?.();
-  }
-
-  closePopup() {
-    if (this.lockScroll) {
-      document.body.style.removeProperty("overflow");
-    }
-
-    if (this.accessibility) {
-      this.openBtn.setAttribute("aria-expanded", "false");
-      this.openBtn.focus();
-    }
-
-    this.popupHolder.classList.remove(this.popupActiveClass);
-
-    this.onClose?.();
-  }
+  // Destroy
+  // Cleanup and reset
 
   destroy() {
     if (this.lockScroll) {
